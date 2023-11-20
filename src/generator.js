cdaae67001da51462ffe1ecef28099f2406d8c52
PerceptControl <egor@dorofeichik.ru>
2023-11-20 08:12:05 +0300

    рефакторинг

--- src/generator.js
+++ src/generator.js
@@ -1,28 +1,18 @@
 const fs = require('node:fs')
 const path = require('node:path')
 
-const { random, min } = require('./utils/math')
+const { min } = require('./utils/math')
 const { STREAM_CAP } = require('./utils/streams')
+const generateString = require('./utils/generateString')
 
 const BYTES_IN_MB = 1024 ** 2
-
-function getString(maxSize) {
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  const buffer = Buffer.alloc(random(1, maxSize))
-  for (let i = 0; i < buffer.byteLength - 1; i++)
-    buffer.write(characters[random(0, characters.length - 1)])
-  buffer.write('\n')
-
-  return buffer
-}
 
 /**
  *
  * @param {object} config
  * @param {(rs: fs.WriteStream) => void} cb
  */
-function generator(config, cb) {
+function Generator(config, cb) {
   if (!config) config = require('/config/base.json')
   if (!config.generator.path) config.path = '/dst'
   if (!config.generator.name || !config.generator.sizeInMb)
@@ -48,7 +38,7 @@ function generator(config, cb) {
     const channel_cap = wstream.highWaterMark - wstream.writableLength
     const file_cap = target_file.totalSize - target_file.writed
 
-    line.data = getString(min(channel_cap, file_cap))
+    line.data = generateString(min(channel_cap, file_cap))
     line.writed = false
   }
 
@@ -56,8 +46,7 @@ function generator(config, cb) {
     while (target_file.writed < target_file.totalSize) {
       if (!wstream.writable) return
       if (line.writed) update_line()
-      console.log(line.data.length, target_file.totalSize)
-      target_file.writed += line.data.length
+      target_file.writed += line.data.length + 1
       if (!wstream.write(line.data, 'utf-8')) return
       line.writed = true
     }
@@ -66,13 +55,10 @@ function generator(config, cb) {
 
   wstream
     .on('ready', () => {
-      console.log('ready')
       start_writing()
       if (target_file.writed >= target_file.totalSize) return void wstream.end()
     })
     .on('drain', () => {
-      console.log('drain')
-      console.log(line.data.length)
       line.writed = true
       if (target_file.writed >= target_file.totalSize) return void wstream.end()
       start_writing()
@@ -83,8 +69,8 @@ function generator(config, cb) {
     })
 }
 
-module.exports = generator
-if (process.env.EXEC) generator()
+module.exports = Generator
+if (process.env.EXEC) Generator()
 if (process.env.DEBUG) {
   console.log('Process RAM: ', process.constrainedMemory() / 1024 ** 2, 'Mb')
   console.log('Process mem: ', process.memoryUsage())
@@ -92,7 +78,7 @@ if (process.env.DEBUG) {
   console.log('Total RAM: ', require('node:os').totalmem() / 1024 ** 2, 'Mb')
 }
 
-generator(
+Generator(
   { generator: { path: 'out', name: 'generated.txt', sizeInMb: 100 } },
   (stream) => {
     console.log('written: ', stream.bytesWritten)
