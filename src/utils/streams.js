9ba81fee919fa17890dd7d3a31d68b447ad934af
PerceptControl <egor@dorofeichik.ru>
2023-11-20 11:27:35 +0300

    полировка и запись идей

--- src/utils/streams.js
+++ src/utils/streams.js
@@ -5,9 +5,11 @@ const events = require('node:events')
 const { min } = require('./math')
 
 const BYTES_IN_MB = 1024 ** 2
-const computedStreamCap = 0.8 * process.memoryUsage.rss()
+const computedStreamCap =
+  0.8 * process.constrainedMemory() ?? process.memoryUsage.rss()
 const maxStreamCap = 100 * BYTES_IN_MB
 const STREAM_CAP = Math.floor(min(computedStreamCap, maxStreamCap))
+
 if (process.env.DEBUG) {
   console.group('Process cap')
   console.log('Computed: ', computedStreamCap)
@@ -60,8 +62,18 @@ async function copyLines(from, to, lines) {
     highWaterMark: STREAM_CAP,
     encoding: 'utf-8',
   })
+
+  /**
+   * алгоритм переноса сделан втупую, поскольку задание, опять же, не боевое
+   * варианты улучшения:
+   * - распараллелить запись и чтение(создавать несколько асинхронных потоков, которые ищут свои строки и затем записывают строки пачкой)
+   * - увеличение работы одного потока чтения(создавать массивы строк сумма размеров которых меньше n и отдавать на поток чтения сразу пачку строк,
+   * таким образом повыситься эффективность отдельного потока и можно будет за один проход по файлу найти больше нужных строк и отправить их на запись
+   * - есть теория что можно в итоговый файл сначала записать \n * на количество строк и дальше параллельно записывать в файл по несколько строк,
+   * поскольку можно гарантировать что на одну \n приходится всего 1 строка. Но это лишь теория, поскольку параллельная запись вероятно будет друг друга перезаписывать)
+   */
   for (let [_, line] of lines) {
-    let status = wstream.write(await readLine(from, line))
+    let status = wstream.write(await readLines(from, line))
     if (!status) await events.once(wstream, 'drain')
   }
 
@@ -71,7 +83,7 @@ async function copyLines(from, to, lines) {
 
 //TODO можно побаловаться с offset'ом, чтобы не пробегать каждый раз по всем строкам, а идти по файлу как по массиву(n + смещение)
 //Но для этого вероятно нельзя использовать readline api. Нужно тестировать и писать через обычный поток в режиме паузы
-function readLine(path, targetPosition) {
+function readLines(path, targetPosition) {
   if (!path || typeof path != 'string') throw Error('wrong path')
   return new Promise((resolve) => {
     const rstream = fs.createReadStream(path, {
@@ -81,14 +93,14 @@ function readLine(path, targetPosition) {
     let currentPosition = 1
     let currentString = null
 
-    rl.on('line', async (line) => {
+    rl.on('line', (line) => {
       line.trim()
       if (line.length == 0) return
       if (currentPosition != targetPosition) return currentPosition++
       currentString = line
       rl.close()
-    }).on('close', async () => {
-      rstream.destroy()
+    }).on('close', () => {
+      rstream.close()
       resolve(currentString + '\n')
     })
   })
